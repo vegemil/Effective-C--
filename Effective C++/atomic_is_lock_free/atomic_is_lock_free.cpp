830ee2a5329fc8d1fb5650f0e5fda726488205b6
으나 :) <euna501@gmail.com>
2016-04-22 11:38:03 +0900

    atomic_is_lock_free
    shared_ptr은 항상 false를 반환함

--- Effective C++/atomic_is_lock_free/atomic_is_lock_free.cpp
+++ Effective C++/atomic_is_lock_free/atomic_is_lock_free.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
-
+#include <memory>
+#include <iostream>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int* p = new int(2);
+	std::shared_ptr<int> sp(p);
+
+	std::cout << (std::atomic_is_lock_free(&sp) ? "true" : "false");
+
 	return 0;
 }
-
